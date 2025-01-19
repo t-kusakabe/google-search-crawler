04e4de25f0a083b6a3304a2a82046f47740a045a
t-kusakabe <ttmac0704@gmail.com>
2025-01-19 18:02:01 +0900

    UA一応設定しておく

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { chromium } from 'playwright';
 const run = async () => {
   const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext({
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    viewport: { width: 1280, height: 720 },
     locale: 'ja-JP',
   });
   const page = await context.newPage();
 
   await page.goto('https://takeout.google.com');
-  
+
   const email: string = process.env.GOOGLE_EMAIL || '';
   await page.fill('input[type="email"]', email);
 
