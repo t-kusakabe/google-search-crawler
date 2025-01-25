867112ab4b5d1dda03659f037f9573ca1cc14e13
t-kusakabe <ttmac0704@gmail.com>
2025-01-26 01:42:03 +0900

    環境変数で管理する

--- src/login/index.ts
+++ src/login/index.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 const run = async () => {
   const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext({
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    userAgent: process.env.USER_AGENT,
     viewport: { width: 1280, height: 720 },
     locale: 'ja-JP',
   });
